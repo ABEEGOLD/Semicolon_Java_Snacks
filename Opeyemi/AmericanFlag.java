32050b71bda06f0d00972dbdbecff2d81214c33c
ABEEGOLD <abeeutee@gmail.com>
2025-05-21 12:26:56 +0100

    Opeyemi

---
+++ Opeyemi/AmericanFlag.java
@@ -0,0 +1,28 @@
+public class AmericanFlag{
+	public static void main(String[] args){
+			
+		int star = 0;
+		int j = 0;
+		for(star=1;star<=7;star++){
+		     for(j=1;j<5;j++){
+			System.out.print("***");
+			}
+			System.out.print("===============");
+			System.out.println("   ");
+			}
+		
+			for(int equals=1;equals<=6;equals++){
+				System.out.print("===");
+
+				for(int i=1;i<9;i++){
+					System.out.print("===");
+				}
+				System.out.print("   ");
+				
+				System.out.println("   ");
+				}
+		
+
+
+	}
+}
